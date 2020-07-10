32bd96a13388c05da91f882ab7411daf2e3a42c5
Karl <karl.roush+github@gmail.com>
2020-07-10 12:07:43 -0400

    progress update

--- main_file.py
+++ main_file.py
@@ -17,6 +17,7 @@ def getAPI_key():
     return file.read()
 
 def make_matchID_list(match_history):
+    #converts t
     matchID_list=[]
     for item in match_history:
         matchID_list.append(item.id)
@@ -34,12 +35,40 @@ def init_playerData(data_fileName):
         "cs_per_min": [],
         "csd_per_min": [],
         "dmgDiff_per_min": [],
-        "xpDiff_per_min": [],
-            }
+        "xpDiff_per_min": []
+        }
     with open(data_fileName, "w") as outfile: 
         json.dump(matchStats, outfile)
         outfile.close()
         
+def getStats(match):
+    # https://readthedocs.org/projects/cassiopeia/downloads/pdf/latest/
+    p = match.participants[summoner]
+    # End of game stats
+    endGame= p.stats
+    # #maybe some kind of vision score weighted by minute? (should be exponential w/ gametime)
+    # Timeline stats; see page 43 of docs
+    # currently only looking at 0-10 min mark
+    timeData= p.timeline
+    cs_per_min = timeData.creeps_per_min_deltas['0-10']
+    csd_per_min= timeData.cs_diff_per_min_deltas['0-10']
+    dmgDiff_per_min= timeData.damage_taken_diff_per_min_deltas['0-10']
+    xpDiff_per_min= timeData.xp_diff_per_min_deltas['0-10']
+    
+    match_stats= {
+        "gold_earned": endGame.gold_earned,
+        "gold_spent": endGame.gold_spent,
+        "total_damage": endGame.total_damage_dealt,
+        "total_damage_champs": endGame.total_damage_dealt_to_champions,
+        "vision_score": endGame.vision_score,
+        "Win": endGame.win,
+        "cs_per_min": cs_per_min,
+        "csd_per_min": csd_per_min,
+        "dmgDiff_per_min": dmgDiff_per_min,
+        "xpDiff_per_min": xpDiff_per_min
+        }
+    return match_stats
+    
 #%% INITIALIZATION
 cass.set_riot_api_key(getAPI_key()) #or replace with your own api key
 cass.set_default_region("NA") #or replace with another region
@@ -58,16 +87,18 @@ summoner = Summoner(name=player_name, region=player_region)
 match_history = summoner.match_history(queues={cass.Queue.ranked_solo_fives})
 matchID_list= make_matchID_list(match_history)
 
+# =============================================================================
 # Current implementation is as follows:
 # 1. Pull the list of match ids
 # 2. Store the most recent matchID (probably index 0) to be cached later
 # 3. for matchID in matchID_list:
-#    3.1. Check IF the current matchID in list is equal to one in cache
+#     3.1. Check IF the current matchID in list is equal to one in cache
 #         3.1.1. True, break from loop and discard current and further matchIDs
 #         3.1.2. False, append to list of new matchIDs that need to be tracked
 # 4. Check to see IF any new matchIDs were actually pulled
-#    4.1. True, set the cache last matchID to be the new last matchID
-#    4.2. False, don't. (Maybe we can skip the rest of the calculation code and just reprint cache stuff if false)
+#     4.1. True, set the cache last matchID to be the new last matchID
+#     4.2. False, don't. (Maybe we can skip the rest of the calculation code and just reprint cache stuff if false)
+# =============================================================================
 new_last_matchID = matchID_list[0]
 new_matchIDs = []
 
@@ -87,51 +118,39 @@ data_fileName= player_name + "_matchStats.json"
 
 checkfile=Path("./"+data_fileName)
 if not checkfile.is_file():
-    print("Player stats file does not exist.")
-    print("Creating stats file...")
+    print("\nPlayer stats file does not exist.")
+    print("Creating initial stats file...")
     init_playerData(data_fileName)
-
-with open(filename, 'r') as openfile: 
-    json_object = json.load(openfile) 
-print(json_object) 
-   
-#%% testing to find right api
-# https://readthedocs.org/projects/cassiopeia/downloads/pdf/latest/
-match = match_history[0]
-# print('Match ID:', match.id)
-p = match.participants[summoner]
-
-# if you want to save memory, write to file instead of variables. 
-# if you want to do analysis in python, save to variable for ease of use
-
-# End of game stats
-endGame= p.stats
-gold_earned= endGame.gold_earned
-gold_spent= endGame.gold_spent
-total_damage= endGame.total_damage_dealt
-total_damage_champs= endGame.total_damage_dealt_to_champions
-vision_score= endGame.vision_score #maybe some kind of vision score weighted by minute? (should be exponential w/ gametime)
-game_outcome= endGame.win
-print("Won game?", game_outcome)
-print("Gold earned=", gold_earned) # can now get all end of game stats
-
-# Timeline stats; see page 43 of docs
-timeData= p.timeline
-cs_per_min = timeData.creeps_per_min_deltas #got it!
-csd_per_min= timeData.cs_diff_per_min_deltas
-dmgDiff_per_min= timeData.damage_taken_diff_per_min_deltas
-xpDiff_per_min= timeData.xp_diff_per_min_deltas
-print("CS diff/min=", csd_per_min)
-
-#%%
-with open(filename, 'r') as openfile: 
-    json_object = json.load(openfile) 
-  
-print(json_object) 
-x= [9,8,7]
-for item in x:
-    json_object["matchIDs"].insert(0,item)
-print(json_object)
+     
+with open(data_fileName, 'r') as openfile: 
+    past_stats = json.load(openfile) 
+    openfile.close()
+    
+# =============================================================================
+# #%% BEGIN ANALYZING MATCHES IN MATCH HISTORY
+# with open("cache.json", 'r') as openfile: 
+#     json_object = json.load(openfile) 
+#     openfile.close()
+# last_analyzed_matchID= json_object["last-matchID"]
+# 
+# #need to prepend stats so that the newest match stats are first in the list!
+# for match in match_history:
+#     if match.id == last_analyzed_matchID:
+#         #reaching the last analyzed match breaks the loop
+#         break
+#     else:
+#         #grab the stats from each match
+#         past_stats["matchIDs"].append(match.id)
+#         match_stats= getStats(match)
+#         for key, value in match_stats.items():    
+#             past_stats[key].append(value)
+#             
+# #%% APPEND THE STATS FROM THE NEW MATCHES TO FILE
+# with open(data_fileName, 'w') as output_data_file: 
+#     json.dump(past_stats, output_data_file) 
+#     output_data_file.close()        
+# 
+# =============================================================================
 #%% WRITE TO CACHE
 # Throughout the script we should be writing to the cache dictionary that was created by reading cache.json at the start
 # Here it is written back to file
