8b363a64dd21eb189068b0213da7c67f6b039a57
ElijahKSmith <squidtheprinter@gmail.com>
2020-07-14 10:56:13 -0400

    Moved main code to an if __name__ == '__main__' block to allow for further expansion of project in the future

--- main_file.py
+++ main_file.py
@@ -103,76 +103,78 @@ def getStats(match):
     
     
     return match_stats
-    
-#%% INITIALIZATION
-cass.set_riot_api_key(getAPI_key()) #or replace with your own api key
-cass.set_default_region("NA") #or replace with another region
 
-with open('cache.json', 'r') as cache_file:
-    cache = json.load(cache_file)
-    cache_file.close()
 
-#%% SETTING THE PLAYER TO BE ANALYZED
-player_name= "RebirthNA"
-player_region= "NA"
-summoner = Summoner(name=player_name, region=player_region)
+if __name__ == '__main__':
+    #%% INITIALIZATION
+    cass.set_riot_api_key(getAPI_key()) #or replace with your own api key
+    cass.set_default_region("NA") #or replace with another region
 
-#%% GET THE MATCH HISTORY
-# for soloQ
-match_history = summoner.match_history(queues={cass.Queue.ranked_solo_fives})
-matchID_list= make_matchID_list(match_history)
+    with open('cache.json', 'r') as cache_file:
+        cache = json.load(cache_file)
+        cache_file.close()
 
-#%% CREATING FILE TO SAVE PLAYER'S STATS
-data_fileName= player_name + "_matchStats.json"
+    #%% SETTING THE PLAYER TO BE ANALYZED
+    player_name= "RebirthNA"
+    player_region= "NA"
+    summoner = Summoner(name=player_name, region=player_region)
 
-new_matchStats= {
-    "MatchID": [None],
-    "gold_earned": [],
-    "gold_spent": [],
-    "total_damage": [],
-    "total_damage_champs": [],
-    "vision_score": [],
-    "Win": [],
-    "cs_per_min": [],
-    "csd_per_min": [],
-    "dmgDiff_per_min": [],
-    "xpDiff_per_min": []
-    }
-    
-checkfile=Path("./"+data_fileName)
-if not checkfile.is_file():
-    print("\nPlayer stats file does not exist.")
-    print("Creating initial stats file...")
-    init_playerData(data_fileName, new_matchStats)
+    #%% GET THE MATCH HISTORY
+    # for soloQ
+    match_history = summoner.match_history(queues={cass.Queue.ranked_solo_fives})
+    matchID_list= make_matchID_list(match_history)
 
-# old match stats     
-with open(data_fileName, 'r') as openfile: 
-    past_stats = json.load(openfile) 
-    openfile.close()
-    
-#%% BEGIN ANALYZING MATCHES IN MATCH HISTORY
-last_analyzed_matchID= past_stats["MatchID"][0]
+    #%% CREATING FILE TO SAVE PLAYER'S STATS
+    data_fileName= player_name + "_matchStats.json"
 
-#need to prepend stats so that the newest match stats are first in the list!
-for match in match_history:
-    if match.id == last_analyzed_matchID:
-        break
-    elif match.duration < timedelta(minutes=3, seconds= 15):
-        # skip remakes
-        pass
-    else:
-        # grab the stats from each match
-        match_stats= getStats(match)
-        for key, value in match_stats.items():  
-            #append the match stats to the new_matchStats dictionary
-            new_matchStats[key].append(value)
-            
-#%% APPEND THE STATS FROM THE NEW MATCHES TO FILE
-for key, value in new_matchStats.items():
-    # combined=itertools.chain(new_stats[key], saved_stats[key])
-    combined= new_matchStats[key]+past_stats[key]
-    past_stats[key]= combined
-    
-with open(data_fileName, 'w') as output_data_file: 
-    json.dump(past_stats, output_data_file) 
-    output_data_file.close()        
+    new_matchStats= {
+        "MatchID": [None],
+        "gold_earned": [],
+        "gold_spent": [],
+        "total_damage": [],
+        "total_damage_champs": [],
+        "vision_score": [],
+        "Win": [],
+        "cs_per_min": [],
+        "csd_per_min": [],
+        "dmgDiff_per_min": [],
+        "xpDiff_per_min": []
+        }
+        
+    checkfile=Path("./"+data_fileName)
+    if not checkfile.is_file():
+        print("\nPlayer stats file does not exist.")
+        print("Creating initial stats file...")
+        init_playerData(data_fileName, new_matchStats)
+
+    # old match stats     
+    with open(data_fileName, 'r') as openfile: 
+        past_stats = json.load(openfile) 
+        openfile.close()
+        
+    #%% BEGIN ANALYZING MATCHES IN MATCH HISTORY
+    last_analyzed_matchID= past_stats["MatchID"][0]
+
+    #need to prepend stats so that the newest match stats are first in the list!
+    for match in match_history:
+        if match.id == last_analyzed_matchID:
+            break
+        elif match.duration < timedelta(minutes=3, seconds= 15):
+            # skip remakes
+            pass
+        else:
+            # grab the stats from each match
+            match_stats= getStats(match)
+            for key, value in match_stats.items():  
+                #append the match stats to the new_matchStats dictionary
+                new_matchStats[key].append(value)
+                
+    #%% APPEND THE STATS FROM THE NEW MATCHES TO FILE
+    for key, value in new_matchStats.items():
+        # combined=itertools.chain(new_stats[key], saved_stats[key])
+        combined= new_matchStats[key]+past_stats[key]
+        past_stats[key]= combined
+        
+    with open(data_fileName, 'w') as output_data_file: 
+        json.dump(past_stats, output_data_file) 
+        output_data_file.close()
